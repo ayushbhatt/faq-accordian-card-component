5c43723e94237191291c4d69507add474241aaac
Ayush Bhatt <51471310+ayushbhatt@users.noreply.github.com>
2022-06-03 20:22:30 +0530

    adding accordian section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'faq-accordian-card-component';
+  acc = document.getElementsByClassName("accordion");
+
+  ngOnInit(){
+    for (let i = 0; i < this.acc.length; i++) {
+      this.acc[i].addEventListener("click", function() {
+      /* Toggle between adding and removing the "active" class,
+      to highlight the button that controls the panel */
+      this.classList.toggle("active");
+
+      /* Toggle between hiding and showing the active panel */
+      var panel = this.nextElementSibling;
+      if (panel.style.display === "block") {
+        panel.style.display = "none";
+      } else {
+        panel.style.display = "block";
+      }
+    });
+  }
+}
 }
